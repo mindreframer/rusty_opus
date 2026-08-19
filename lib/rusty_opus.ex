c27ef174234d50b1b428eb03c8ed9470b7cdcdea
Roman Heinrich <roman.heinrich@gmail.com>
2026-08-19 15:59:44 +0200

    init

---
+++ lib/rusty_opus.ex
@@ -0,0 +1,18 @@
+defmodule RustyOpus do
+  @moduledoc """
+  Documentation for `RustyOpus`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> RustyOpus.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/rusty_opus_test.exs
@@ -0,0 +1,8 @@
+defmodule RustyOpusTest do
+  use ExUnit.Case
+  doctest RustyOpus
+
+  test "greets the world" do
+    assert RustyOpus.hello() == :world
+  end
+end
